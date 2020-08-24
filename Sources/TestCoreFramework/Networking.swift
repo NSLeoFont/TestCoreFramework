f6f95b07ec99327191d241fb6c64d6764a3516f4
Leo Font <leofontdev@gmail.com>
2020-08-24 21:06:21 +0100

    Added first networking API for retrieving network data

--- Sources/TestCoreFramework/Networking.swift
+++ Sources/TestCoreFramework/Networking.swift
@@ -8,9 +8,33 @@
 import Foundation
 
 extension TestCoreFramework {
-    
+
     public class Networking {
         
+        public enum NetworkingResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
+        
+        /// Responsible for handling all networking class
+        /// -Warning: Must create before any public APIs
+        public class Manager {
+            
+            private let session = URLSession.shared
+            
+            public init() {}
+            
+            public func loadData(from url: URL, completion: @escaping (NetworkingResult<Data>) -> Void)  {
+                
+                let task = session.dataTask(with: url) { data, response, error in
+                    let result = data.map(NetworkingResult<Data>.success) ?? .failure(error)
+                    completion(result)
+                }
+                task.resume()
+                
+                
+            }
+        }
         
     }
 }
--- Tests/TestCoreFrameworkTests/TestCoreFrameworkNetworkingTests.swift
+++ Tests/TestCoreFrameworkTests/TestCoreFrameworkNetworkingTests.swift
@@ -6,16 +6,35 @@
 //
 
 import XCTest
+@testable import TestCoreFramework
 
 final class TestCoreFrameworkNetworkingTests: XCTestCase {
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testLoadDataCall() {
+        let manager = TestCoreFramework.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        
+        guard let url = URL(string: "https://www.leofont.eu") else {
+            return XCTFail("Could not create URL properly")
+        }
+        
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            
+            switch result {
+            case .success(let returnedData):
+                XCTAssertNil(returnedData, "Returned data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "There was an error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+        
+        
     }
 
     static var allTests = [
-        ("testExample", testExample)
+        ("testLoadDataCall", testLoadDataCall)
         
     ]
     
